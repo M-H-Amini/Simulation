3c3eca2cd955b151faec455d2ba07b452ca53672
M-H-Amini <m.hossein.amini@gmail.com>
2022-09-05 22:32:38 +0430

    cleaner code :)

--- simulation.py
+++ simulation.py
@@ -12,7 +12,14 @@ import time
 import pybullet_data
 import numpy as np
 
-def move(p, q, angle, v):
+def move(q, angle=0., v=5):
+    """Moves the q robot in a given angle with a given velocity
+
+    Args:
+        q (int): Robot integer identifier
+        angle (float, optional): Direction angle in radian. Defaults to 0..
+        v (int, optional): Velocity. Defaults to 5.
+    """
     p.setJointMotorControl2(
         q, 2, p.VELOCITY_CONTROL, targetVelocity=v * (1 + np.sin(angle))
     )
@@ -26,16 +33,13 @@ def move(p, q, angle, v):
         q, 5, p.VELOCITY_CONTROL, targetVelocity=v * (1 - np.sin(angle))
     )
 
-N_bot = 3
+N_bot = 3  ##  Number of huskies
 time.sleep(2)
 ##  Simulator
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
-print('here', pybullet_data.getDataPath())
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -10)
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0, 0, 1]
-cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 
 husky = [p.loadURDF(
     "husky/husky.urdf", [0, (i - N_bot/2) * 1.5, 0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=False
@@ -48,19 +52,15 @@ traybox = [p.loadURDF(
 ) for i in range(len(obstacle_pos))
 ]
 
-
-angles = {"rf": 0, "lf": 0, "rb": 0, "lb": 0}
-
 nJ = p.getNumJoints(husky[0])
 print("No of joints: ", p.getNumJoints(husky[0]))
 for i in range(nJ):
     print("Joint info: ", p.getJointInfo(husky[0], i))
 print("No of constraints: ", p.getNumConstraints())
-prev_pos, orient = p.getBasePositionAndOrientation(husky[0])
 
 for i in range(5000000):
-    if i > 200:
-        [move(p, husky[i], np.pi / (i+1) * np.cos(np.pi * (i+1)), np.random.randint(4,7)) for i in range(N_bot)]
+    if i > 100:
+        [move(husky[i], np.pi / (i+1) * np.cos(np.pi * (i+1)), np.random.randint(4,7)) for i in range(N_bot)]
     p.stepSimulation()
     time.sleep(1.0 / 240.0)
 
